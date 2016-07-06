4fe43b0e03fa6d9635dd32689dde68af5c6f65ed
marcophelan <marcophelan@users.noreply.github.com>
2016-07-06 19:20:49 -0400

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object and displays the 
+## mean value
 
-## Write a short comment describing this function
+## This function creates the special vector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse<-NULL
+        base<-function(y){
+                x<<-y
+                inverse<-NULL
+        }
+        receive<-function() x
+        baseInverse<-function(inverse) inverse<<-inverse
+        receiveInverse<-function()inverse
+        list(base=base, receive=receive, baseInverse=baseInverse, receiveInverse=receiveInverse
 }
 
 
-## Write a short comment describing this function
+## This function gets the inverse of the special matrix above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse<-x$receiveInverse()
+        if(!is.null(inverse)){
+        messsage("Getting Cached Data")
+        return(inverse)
+        }
+        numbers<-x$receive()
+        inverse<-solve(data,...)
+        x$baseInverse(inverse)
+        inverse
+        }
+        
 }
